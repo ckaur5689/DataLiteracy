925ab67af5233b302acf11ae6a5fdd0b9910d10a
Chandan Kaur <ckaur5689@gmail.com>
2026-07-09 09:00:17 +0100

    Fix indentation in app.py

--- coach/app.py
+++ coach/app.py
@@ -116,7 +116,7 @@ st.markdown(
     "</p>",
     unsafe_allow_html=True
 )
-    col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns([1,2,1])
 
 with col2:
     recommend = st.button(
